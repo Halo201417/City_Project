3c9acc65e5995e3c976ee521971424aeeb5ca0bf
Jose <josmamogut@gmail.com>
2025-11-16 13:45:16 +0100

    modified:   contact.html 	modified:   css/style.css 	modified:   events.html 	modified:   history.html 	modified:   index.html 	modified:   js/script.js 	modified:   places.html 	modified:   services.html

--- js/script.js
+++ js/script.js
@@ -2,6 +2,39 @@ document.addEventListener('DOMContentLoaded', (event) =>{
     const lightbox = GLightbox({
         loop:true,
         touchNavigation: true,
-        descPosition: bottom
+        descPosition: 'bottom'
+    });
+
+    //Button
+    const backToTopBtn = document.getElementById("backToTopBtn");
+
+    const scrollFunction = () => {
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
+            backToTopBtn.style.display = "block";
+        }
+        else{
+            backToTopBtn.style.display = "none";
+        }
+    };
+
+    const topFunction = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    };
+
+    window.onscroll = () => scrollFunction();
+    backToTopBtn.onclick = () => topFunction();
+
+    const currentPage = window.location.pathname;
+    const navLinks = document.querySelectorAll('.global-nav li a');
+
+    navLinks.forEach(link => {
+        const linkHref = link.getAttribute('href');
+
+        if(currentPage.endsWith(linkHref)){
+            link.classList.add('active');
+        }
     });
 });
